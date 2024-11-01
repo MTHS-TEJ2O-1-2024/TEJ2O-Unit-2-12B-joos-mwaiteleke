5d38c136492a35c38bc6bc283a86f55848ddd5d2
joos-mwaiteleke <joos.mwaiteleke@mths.ca>
2024-11-01 11:14:04 -0400

    This program turns on neopixels based on distance using sonar

--- main.ts
+++ main.ts
@@ -6,20 +6,37 @@
 */
 
 let distance = 0
-let strip = neopixel.create(DigitalPin.P0, 10, NeoPixelMode.RGB) // Adjust DigitalPin and length to match your setup
+let neopixelStrip: neopixel.Strip = null
+
+basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
     // Measure the distance
-    distance = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Centimeters) // Adjust pins to match your setup
+    input.onButtonPressed(Button.A, function () {
+        distance = sonar.ping(
+            DigitalPin.P1,
+            DigitalPin.P2,
+            PingUnit.Centimeters
+        )
 
     // Turns on neopixels based on distance
     if (distance < 10) {
-        // Turn NeoPixels to red if distance is less than 10 cm
-        strip.showColor(neopixel.colors(NeoPixelColors.Red))
-    } else {
-        // Turn NeoPixels to green if distance is 10 cm or more
-        strip.showColor(neopixel.colors(NeoPixelColors.Green))
-    }
-    strip.show()
-})
 
+        // Turn NeoPixels to red if distance is less than 10 cm
+        neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.show()
+
+    } else {
+
+        // Turn NeoPixels to green if distance is 10 cm or more
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.show()
+    }
+})})
